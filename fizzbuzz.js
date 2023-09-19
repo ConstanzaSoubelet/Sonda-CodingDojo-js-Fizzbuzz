4a0b7ecf5c0508d7d86ddfb0d93a014dbcd3b7d6
ConstanzaSoubelet <conniesoubelet@gmail.com>
2023-09-19 16:53:37 -0300

    subir js

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+
+// utilizando un bucle for y condicionales if-else.
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
+
+
+//ejecutar programa: node fizzbuzz.js
